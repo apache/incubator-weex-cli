4f2298bff6f57b8458d758443dd2b1b4d7ac3848
zikuan.ly <zikuan.ly@taobao.com>
2016-07-07 12:39:02 +0800

    * [toolkit] filter out local IP

--- build/nw-utils.js
+++ build/nw-utils.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+    value: true
 });
 exports.getPublicIP = getPublicIP;
 /*
@@ -12,14 +12,14 @@ var os = require('os');
 var _ = require("underscore");
 
 function getPublicIP() {
-	var publicIP = "127.0.0.1"; //fallbck ip
-	var ifaces = os.networkInterfaces();
-	var address = _.flatten(_.values(ifaces));
-	address = _.filter(address, function (ifObj) {
-		return ifObj.family == "IPv4" && ifObj.address != "127.0.0.1";
-	});
-	if (address.length > 0) {
-		publicIP = address[0].address;
-	}
-	return publicIP;
+    var publicIP = "127.0.0.1"; //fallbck ip
+    var ifaces = os.networkInterfaces();
+    var address = _.flatten(_.values(ifaces));
+    address = _.filter(address, function (ifObj) {
+        return ifObj.family == "IPv4" && !/^127\./.test(ifObj.address);
+    });
+    if (address.length > 0) {
+        publicIP = address[0].address;
+    }
+    return publicIP;
 }
--- src/nw-utils.js
+++ src/nw-utils.js
@@ -9,7 +9,12 @@ export function getPublicIP(){
     let publicIP = "127.0.0.1"  //fallbck ip
 	let ifaces = os.networkInterfaces()
 	let address = _.flatten(_.values(ifaces))
-	address = _.filter( address , (ifObj) =>  ( (ifObj.family == "IPv4") && (ifObj.address != "127.0.0.1") ) )
+	address = _.filter( address , (ifObj) =>
+                        (
+                            (ifObj.family == "IPv4") &&
+                            (  ! /^127\./.test(ifObj.address)  )
+                        )
+                      )
 	if (address.length > 0 ){
 	    publicIP = address[0].address
 	}
