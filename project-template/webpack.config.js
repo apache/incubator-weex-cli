aa497e8aeb061df32c66ed506fd154c1c1184197
esmeetu <jasonailu87@hotmail.com>
2016-10-19 15:52:26 +0800

    update template

--- project-template/webpack.config.js
+++ project-template/webpack.config.js
@@ -1,21 +1,39 @@
-require('webpack')
-require('weex-loader')
+var path = require('path');
+var fs = require('fs');
 
-var path = require('path')
+var entry = {};
+
+function walk(dir) {
+  dir = dir || '.'
+  var directory = path.join(__dirname, 'src', dir);
+  fs.readdirSync(directory)
+    .forEach(function(file) {
+      var fullpath = path.join(directory, file);
+      var stat = fs.statSync(fullpath);
+      var extname = path.extname(fullpath);
+      if (stat.isFile() && extname === '.we') {
+        var name = path.join('dist', dir, path.basename(file, extname));
+        entry[name] = fullpath + '?entry=true';
+      } else if (stat.isDirectory() && file !== 'build' && file !== 'include') {
+        var subdir = path.join(dir, file);
+        walk(subdir);
+      }
+    });
+}
+
+walk();
 
 module.exports = {
-  entry: {
-    main: path.join(__dirname, 'src', 'main.we?entry=true')
-  },
-  output: {
-    path: 'dist',
+  entry: entry,
+  output : {
+    path: '.',
     filename: '[name].js'
   },
   module: {
     loaders: [
       {
         test: /\.we(\?[^?]+)?$/,
-        loaders: ['weex-loader']
+        loader: 'weex'
       }
     ]
   }
