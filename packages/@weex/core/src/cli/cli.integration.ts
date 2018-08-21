8379e4cbd54a355780246a010bca26dae759e49e
erha19 <faterrole@gmail.com>
2018-08-21 21:44:32 +0800

    async remove trash

--- packages/@weex/core/src/cli/cli.integration.ts
+++ packages/@weex/core/src/cli/cli.integration.ts
@@ -1,83 +1,79 @@
-import * as expect from 'expect'
-import * as sinon from 'sinon'
-import * as uniqueTempDir from 'unique-temp-dir'
-import * as stripANSI from 'strip-ansi'
-import { run as cli } from './cli'
+// import { run as cli } from './cli'
 
-sinon.stub(console, 'log')
+// sinon.stub(console, 'log')
 
-const pwd = process.cwd()
+// const pwd = process.cwd()
 
-// set jest timeout to very long, because these take a while
-beforeAll(() => jest.setTimeout(90 * 1000))
-// reset back
-afterAll(() => jest.setTimeout(5 * 1000))
+// // set jest timeout to very long, because these take a while
+// beforeAll(() => jest.setTimeout(90 * 1000))
+// // reset back
+// afterAll(() => jest.setTimeout(5 * 1000))
 
-test('can start the cli', async () => {
-  const c = await cli()
-  expect(c).toBeTruthy()
-})
+// test('can start the cli', async () => {
+//   const c = await cli()
+//   expect(c).toBeTruthy()
+// })
 
-test('can create a new boilerplate cli', async () => {
-  const tmp = uniqueTempDir({ create: true })
-  process.chdir(tmp as string)
+// test('can create a new boilerplate cli', async () => {
+//   const tmp = uniqueTempDir({ create: true })
+//   process.chdir(tmp as string)
 
-  const toolbox = await cli('new foo')
-  expect(toolbox.command.name).toBe('new')
+//   const toolbox = await cli('new foo')
+//   expect(toolbox.command.name).toBe('new')
 
-  const pkg = toolbox.fs.read(`${tmp}/foo/package.json`, 'json')
+//   const pkg = toolbox.fs.read(`${tmp}/foo/package.json`, 'json')
 
-  expect(typeof pkg).toBe('object')
-  expect(pkg.name).toBe('foo')
-  expect(pkg.private).toBeTruthy()
-  expect(Object.keys(pkg.dependencies).includes('gluegun')).toBeTruthy()
+//   expect(typeof pkg).toBe('object')
+//   expect(pkg.name).toBe('foo')
+//   expect(pkg.private).toBeTruthy()
+//   expect(Object.keys(pkg.dependencies).includes('gluegun')).toBeTruthy()
 
-  // Install local version of gluegun to test
-  await toolbox.system.run(`cd ${tmp}/foo && npm install ${pwd}`)
+//   // Install local version of gluegun to test
+//   await toolbox.system.run(`cd ${tmp}/foo && npm install ${pwd}`)
 
-  // Try running the help command, see what it does
-  const runCommand = await toolbox.system.run(`${tmp}/foo/bin/foo --help`)
-  expect(stripANSI(runCommand)).toMatchSnapshot()
+//   // Try running the help command, see what it does
+//   const runCommand = await toolbox.system.run(`${tmp}/foo/bin/foo --help`)
+//   expect(stripANSI(runCommand)).toMatchSnapshot()
 
-  // Try running the generate command, see what it does
-  const genCommand = await toolbox.system.run(`${tmp}/foo/bin/foo g model test`)
-  expect(stripANSI(genCommand)).toMatchSnapshot()
+//   // Try running the generate command, see what it does
+//   const genCommand = await toolbox.system.run(`${tmp}/foo/bin/foo g model test`)
+//   expect(stripANSI(genCommand)).toMatchSnapshot()
 
-  // clean up
-  process.chdir(pwd)
-  toolbox.fs.remove(`${tmp}/foo`)
-})
+//   // clean up
+//   process.chdir(pwd)
+//   toolbox.fs.remove(`${tmp}/foo`)
+// })
 
-test('can create a new boilerplate TypeScript cli', async () => {
-  const tmp = uniqueTempDir({ create: true })
-  process.chdir(tmp as string)
+// test('can create a new boilerplate TypeScript cli', async () => {
+//   const tmp = uniqueTempDir({ create: true })
+//   process.chdir(tmp as string)
 
-  const toolbox = await cli('new foo-ts --typescript')
-  expect(toolbox.command.name).toBe('new')
+//   const toolbox = await cli('new foo-ts --typescript')
+//   expect(toolbox.command.name).toBe('new')
 
-  const pkg = toolbox.fs.read(`${tmp}/foo-ts/package.json`, 'json')
+//   const pkg = toolbox.fs.read(`${tmp}/foo-ts/package.json`, 'json')
 
-  expect(typeof pkg).toBe('object')
-  expect(pkg.name).toBe('foo-ts')
-  expect(pkg.private).toBeTruthy()
-  expect(Object.keys(pkg.dependencies).includes('gluegun')).toBeTruthy()
+//   expect(typeof pkg).toBe('object')
+//   expect(pkg.name).toBe('foo-ts')
+//   expect(pkg.private).toBeTruthy()
+//   expect(Object.keys(pkg.dependencies).includes('gluegun')).toBeTruthy()
 
-  // Install local version of gluegun to test
-  await toolbox.system.run(`cd ${tmp}/foo-ts && npm install ${pwd}`)
+//   // Install local version of gluegun to test
+//   await toolbox.system.run(`cd ${tmp}/foo-ts && npm install ${pwd}`)
 
-  // Try running the help command, see what it does
-  const runCommand = await toolbox.system.run(`${tmp}/foo-ts/bin/foo-ts --help`)
-  expect(stripANSI(runCommand)).toMatchSnapshot()
+//   // Try running the help command, see what it does
+//   const runCommand = await toolbox.system.run(`${tmp}/foo-ts/bin/foo-ts --help`)
+//   expect(stripANSI(runCommand)).toMatchSnapshot()
 
-  // Try running the generate command, see what it does
-  const genCommand = await toolbox.system.run(`${tmp}/foo-ts/bin/foo-ts g model test`)
-  expect(stripANSI(genCommand)).toMatchSnapshot()
+//   // Try running the generate command, see what it does
+//   const genCommand = await toolbox.system.run(`${tmp}/foo-ts/bin/foo-ts g model test`)
+//   expect(stripANSI(genCommand)).toMatchSnapshot()
 
-  // Run that command and check the result
-  const kitchenCommand = await toolbox.system.run(`${tmp}/foo-ts/bin/foo-ts kitchen`)
-  console.log(kitchenCommand)
+//   // Run that command and check the result
+//   const kitchenCommand = await toolbox.system.run(`${tmp}/foo-ts/bin/foo-ts kitchen`)
+//   console.log(kitchenCommand)
 
-  // clean up
-  process.chdir(pwd)
-  toolbox.fs.remove(`${tmp}/foo-ts`)
-})
+//   // clean up
+//   process.chdir(pwd)
+//   toolbox.fs.remove(`${tmp}/foo-ts`)
+// })
