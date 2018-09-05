2a9c1aca354d2f4619b6d8a27124ce5e864d21f9
balloonzzq <zqzhuang888@qq.com>
2018-09-05 10:54:57 +0800

    feat: generator

--- packages/@weex/plugins/generator/README.md
+++
@@ -1,1 +0,0 @@
-Plugin for generate templates.
---
+++ packages/@weex/plugins/generator/lib/download.js
@@ -0,0 +1,13 @@
+// import { download } from 'download-git-repo';
+// console.log('downloadGitRepo', downloadGitRepo);
+// import * as path from "path";
+// export function download(target: string): Promise<string> {
+//   target = path.join(target || '.', '.download-temp');
+//   console.log('download-target', target);
+//   return new Promise((resolve, reject) => {
+//     download('https://github.com/weex-templates/webpack.git#master', target, {}, (err) => {
+//       console.log(err ? 'Error' : 'Success')
+//     });
+//   });
+// }
+//# sourceMappingURL=download.js.map
