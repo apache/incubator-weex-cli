e4a0aecde107ced421ef47c7b83b26f433ccf9f7
balloonzzq <zqzhuang888@qq.com>
2018-09-07 15:20:59 +0800

    feat: readme

--- packages/@weex/plugins/generator/index.d.ts
+++ packages/@weex/plugins/generator/index.d.ts
@@ -6,6 +6,6 @@ declare module '@weex-cli/generator' {
   export interface CloneOption {
     cache: boolean;
   }
-  export function generator(projectName: string, source: string, dest:string, metadata: Metadate): Promise<boolean>;
+  export function generator(source: string, dest:string, metadata: Metadate): Promise<boolean>;
   export function clone(templateUrl: string, target?: string, option?: CloneOption): Promise<string>;
 }
--- packages/@weex/plugins/generator/lib/index.js
+++ packages/@weex/plugins/generator/lib/index.js
@@ -7,15 +7,13 @@ const fs = require("fs");
 const render_1 = require("./render");
 const rimraf = require("rimraf");
 const defautlTarget = path.join(path.dirname(__dirname), index_1.TEMPLATE_NAME);
-function generator(projectName, source, dest, metadata) {
-    console.log('generator.ts', projectName, source, dest);
+function generator(source, dest, metadata) {
     return new Promise((resolve, reject) => {
         render_1.default(source, dest, metadata);
     });
 }
 exports.generator = generator;
 function clone(templateUrl, target = defautlTarget, option) {
-    console.log('templateUrl', templateUrl, target);
     return new Promise((resolve, reject) => {
         if (option && option.cache) {
             resolve(target);
@@ -31,7 +29,6 @@ function clone(templateUrl, target = defautlTarget, option) {
         function done() {
             download(templateUrl, target, { clone: true }, (err) => {
                 // download('direct:https://github.com/balloonzzq/webpack.git#temp', target, { clone: true }, (err) => {
-                console.log(err ? 'downloadError' : 'Success');
                 if (err) {
                     reject(err);
                 }
