20bd5d1b0c78a27a8711a8c8dee8da77ef41868a
balloonzzq <zqzhuang888@qq.com>
2018-09-27 15:35:20 +0800

    fix: update export doctor and readme

--- packages/@weex/plugins/doctor/README.md
+++ packages/@weex/plugins/doctor/README.md
@@ -1,2 +1,14 @@
 ### @weex-cli/doctor
+Provide doctor module detection weex development environment
 
+### Install
+```
+npm install @weex-cli/doctor --save
+```
+
+### Usage
+```
+const Doctor = require('@weex-cli/doctor').Doctor
+new Doctor().diagnose()
+
+```
--- packages/@weex/plugins/doctor/lib/index.js
+++ packages/@weex/plugins/doctor/lib/index.js
@@ -1,11 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const doctor_1 = require("./doctor");
-function default_1() {
-    return new Promise((resolve, reject) => {
-        const doctor = new doctor_1.Doctor();
-        resolve(doctor);
-    });
-}
-exports.default = default_1;
+var doctor_1 = require("./doctor");
+exports.Doctor = doctor_1.Doctor;
 //# sourceMappingURL=index.js.map
