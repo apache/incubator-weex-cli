a73b238959f8a58c8ece44a44ca7f5af00bef635
erha19 <faterrole@gmail.com>
2018-11-19 15:18:51 +0800

    [lint] add lint package

---
+++ packages/@weex/plugins/lint/src/index.ts
@@ -0,0 +1,1 @@
+export { default as Linter } from './linter'
---
+++ packages/@weex/plugins/lint/src/linter.ts
@@ -0,0 +1,46 @@
+import { CLIEngine } from 'eslint'
+
+export default class WeexLinter {
+  private engine: CLIEngine
+  
+  private defaultOptions: CLIEngine.Options = {
+    useEslintrc: false,
+    extensions: [
+      '.js',
+      '.vue'
+    ],
+    baseConfig: {
+      extends: [
+        'eslint-config-weex/vue'
+      ]
+    }
+  }
+
+  constructor(options: CLIEngine.Options) {
+    this.engine = new CLIEngine(Object.assign(options, this.defaultOptions))
+  }
+
+  public executeOnFiles (files) {
+    return this.engine.executeOnFiles(files)
+  }
+
+  public executeOnText (sourcecode) {
+    return this.engine.executeOnText(sourcecode)
+  }
+
+  public getConfigForFile (printConfig) {
+    return this.engine.getConfigForFile(printConfig)
+  }
+
+  public outputFixes(report) {
+    return CLIEngine.outputFixes(report)
+  }
+
+  public getErrorResults(report) {
+    return CLIEngine.getErrorResults(report)
+  }
+
+  public getFormatter(code) {
+    return this.engine.getFormatter(code)
+  }
+} 
