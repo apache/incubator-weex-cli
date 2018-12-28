3f300a82735a41414d36dd685444b993bf92d735
kuiwu <danwu.wdw@alibaba-inc.com>
2018-12-28 15:46:24 +0800

    [build] release v1.0.3

--- packages/@weex/plugins/build/commands/build.js
+++ packages/@weex/plugins/build/commands/build.js
@@ -14,12 +14,17 @@ module.exports = {
       compile
     }
   ) => {
-    logger.log('Packaging a project for preview is meaningless.')
+    logger.log('Packaging a project which is used for preview is meaningless.')
     logger.log('We are thinking about how to better define this module.')
     logger.log('Please do not use this command for the time being.')
 
     logger.log('\nFor more infomations, you can see:')
 
     logger.log(logger.colors.grey('https://github.com/weexteam/weex-toolkit/blob/master/README.md'))
+  
+    logger.log('\nOr you can give us some advice here:')
+
+    logger.log(logger.colors.grey('https://github.com/weexteam/weex-toolkit/issues/new?labels=@weex-cli/build'))
+    
   }
 };
