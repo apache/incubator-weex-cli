90b709031a6f6b9e885e47167cf8b779b2902b73
erha19 <faterrole@gmail.com>
2019-01-03 22:16:39 +0800

    feat(cli): add integration test

--- bin/weex.js
+++ bin/weex.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
-
 "use strict";
+
+// first, do a sniff test to ensure our dependencies are met
+const sniff = require('../lib/sniff')
+const colors = require('colors')
+
+// check the node version
+if (!sniff.isNewEnough) {
+  console.log('Node.js 7.6.0 + is required to run. You have ' + sniff.nodeVersion + '.');
+  console.log(colors.grey('You can install the latest version here: https://nodejs.org/en/'))
+  process.exit(1);
+}
 
 require('../lib/cli')(process.argv);
---
+++ scripts/testSetup.js
@@ -0,0 +1,1 @@
+process.env.WEEX_CLI_TEST = true
