4094927b4e6eedd75f21da83d604957fffee5b5e
erha19 <faterrole@gmail.com>
2019-02-25 14:32:51 +0800

    bugfix(debug): fix SSRF issue

--- packages/@weex/plugins/debug/src/server/router/http.js
+++ packages/@weex/plugins/debug/src/server/router/http.js
@@ -2,13 +2,7 @@ const Router = require('koa-router')
 const MemoryFile = require('../../MemoryFile')
 const mlink = require('../../link')
 const DeviceManager = require('../../link/managers/device_manager')
-const URL = require('url')
 const config = require('../../config')
-const protocols = {
-  'http:': require('http'),
-  'https:': require('https')
-}
-const { logger } = require('../../util')
 
 const httpRouter = new Router()
 
@@ -18,72 +12,18 @@ const SyncTerminal = mlink.Terminal.SyncTerminal
 const syncNativeHub = mlink.Hub.get('sync.native')
 const syncV8Hub = mlink.Hub.get('sync.v8')
 
-const rSourceMapDetector = /\.map$/
-
-const getRemote = url => {
-  return new Promise(function (resolve, reject) {
-    const urlObj = URL.parse(url)
-    ;(protocols[urlObj.protocol] || protocols['http:'])
-      .get(
-        {
-          hostname: urlObj.hostname,
-          port: urlObj.port,
-          path: urlObj.path,
-          method: 'GET',
-          headers: {
-            'User-Agent': 'Weex/1.0.0'
-          }
-        },
-        function (res) {
-          let chunks = []
-          res.on('data', function (chunk) {
-            chunks.push(chunk)
-          })
-          res.on('end', function () {
-            resolve(Buffer.concat(chunks).toString())
-            chunks = null
-          })
-        }
-      )
-      .on('error', function (e) {
-        reject(e)
-      })
-  })
-}
-
 httpRouter.get('/source/*', async (ctx, next) => {
   const path = ctx.params[0]
-  if (rSourceMapDetector.test(path)) {
-    logger.verbose(`Fetch sourcemap ${path}`)
-    let content
-    try {
-      content = await getRemote('http://' + path)
-    }
-    catch (e) {
-      logger.verbose(`Failed to fetch, reason: ${e.stack}`)
-    }
-    if (!content) {
-      ctx.response.status = 404
-    }
-    else {
-      ctx.response.status = 200
-      ctx.type = 'text/javascript'
-      ctx.set('Access-Control-Allow-Origin', '*')
-      ctx.response.body = content
-    }
+  let query = ctx.request.url.split('?')
+  query = query[1] ? '?' + query.slice(1).join('?') : ''
+  const file = MemoryFile.get(path + query)
+  if (file) {
+    ctx.response.status = 200
+    ctx.type = 'text/javascript'
+    ctx.response.body = file.getContent()
   }
   else {
-    let query = ctx.request.url.split('?')
-    query = query[1] ? '?' + query.slice(1).join('?') : ''
-    const file = MemoryFile.get(path + query)
-    if (file) {
-      ctx.response.status = 200
-      ctx.type = 'text/javascript'
-      ctx.response.body = file.getContent()
-    }
-    else {
-      ctx.response.status = 404
-    }
+    ctx.response.status = 404
   }
   await next()
 })
