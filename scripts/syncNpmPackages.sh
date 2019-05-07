4562d54fe07091f793073bdb519713bbca4cd714
erha19 <faterrole@gmail.com>
2019-05-07 18:34:35 +0800

    workflow(script): add script to sync taobao npm origin

---
+++ scripts/syncNpmPackages.sh
@@ -0,0 +1,3 @@
+# need install cnpm first
+# npm i cnpm -g
+cnpm sync weex-toolkit @weex-cli/core @weex-cli/compile @weex-cli/plugin @weex-cli/doctor @weex-cli/build @weex-cli/run @weex-cli/debug @weex-cli/lint @weex-cli/generator @weex-cli/device
